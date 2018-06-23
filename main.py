867609c07286e1f731c6a9baad329bdff2c27267
Charles-IV <cthefourth@gmail.com>
2018-06-23 19:33:20 +0100

    don't remove old role
    unless promoting from recruit.
    People can be more than one role at once, and this means people don't have to follow a specific role path without breaking stuff.

--- main.py
+++ main.py
@@ -36,10 +36,8 @@ async def on_message(message):
                 await client.send_message(message.server.get_channel('459992736662290442'),"Congratulations " + data[1] + ", you have been promoted to . You have proved to be worthy of this title over the time you have spent as a *Programmer*. We implore you to help others and demonstrate your knowledge by helping others to succeed.")
                 memID = data[1].strip("<@").strip(">")
                 mem = message.server.get_member(memID)
-                role1 = discord.utils.get(message.server.roles, name="Programmer")
-                role2 = discord.utils.get(message.server.roles, name="Expert")
-                await client.remove_roles(mem, role1)
-                await client.add_roles(mem, role2)
+                role = discord.utils.get(message.server.roles, name="Expert")
+                await client.add_roles(mem, role)
                 
         else:
             await client.send_message(message.channel, "Please specify user")
@@ -52,10 +50,8 @@ async def on_message(message):
                 await client.send_message(message.server.get_channel('459992736662290442'),"Congratulations " + data[1] + ", you have been promoted to become a member of the @&Management team. You have proved to be worthy of this title over the time you have spent as an Expert. We implore you to help others and demonstrate your knowledge by helping others to succeed, while keeping the server in good running order and carrying out the tasks required of you. Should you not wish to undertake this role, contact <@334376933771182080>, and you will be promoted to *Senior Expert*, showing that you have the required level of knowledge for *Management*, but do not wish to have the same levels of responsibility.")
                 memID = data[1].strip("<@").strip(">")
                 mem = message.server.get_member(memID)
-                role1 = discord.utils.get(message.server.roles, name="Expert")
-                role2 = discord.utils.get(message.server.roles, name="Management")
-                await client.remove_roles(mem, role1)
-                await client.add_roles(mem, role2)
+                role = discord.utils.get(message.server.roles, name="Management")
+                await client.add_roles(mem, role)
                 
         else:
             await client.send_message(message.channel, "Please specify user")
@@ -68,10 +64,8 @@ async def on_message(message):
                 await client.send_message(message.server.get_channel('459992736662290442'),"Congratulations " + data[1] + ", you have been promoted to become a *Senior Expert*. You have proved your expertise time and time again and because of this <@334376933771182080> has decided to promote you to *Senior Expert*. Please demonstrate your immense bank of knowledge by helping others and producing awesome code.")
                 memID = data[1].strip("<@").strip(">")
                 mem = message.server.get_member(memID)
-                role1 = discord.utils.get(message.server.roles, name="Expert")
-                role2 = discord.utils.get(message.server.roles, name="Senior Expert")
-                await client.remove_roles(mem, role1)
-                await client.add_roles(mem, role2)
+                role = discord.utils.get(message.server.roles, name="Senior Expert")
+                await client.add_roles(mem, role)
                 
         else:
             await client.send_message(message.channel, "Please specify user")
